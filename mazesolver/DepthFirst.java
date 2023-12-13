8f8d11819059c112e05b5791c4228221ab4fa268
elsaneth <elisabeth.suits@gmail.com>
2023-12-13 21:34:35 +0200

    use points instead of array

--- mazesolver/DepthFirst.java
+++ mazesolver/DepthFirst.java
@@ -1,29 +1,29 @@
 package mazesolver;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class DepthFirst {
 
-    public static List<Integer> getPath(int[][] maze) {
+    public static List<Point> getPoints(int[][] maze) {
         int[][] mazeClone = new int[maze.length][];
         for (int i = 0; i < maze.length; i++) {
             mazeClone[i] = maze[i].clone();
         }
-        List<Integer> mutablePath = new ArrayList<>();
+
+        List<Point> results = new ArrayList<>();
         long startNano = System.nanoTime();
-        DepthFirst.searchPath(mazeClone, 1, 1, mutablePath);
-        Collections.reverse(mutablePath);
+        DepthFirst.searchPath(mazeClone, 1, 1, results);
+        Collections.reverse(results);
         long endNano = System.nanoTime();
-        System.out.println("Breadth First Search algorithm took " + (endNano - startNano) + " nanoseconds.");
-        return mutablePath;
+        System.out.println("Depth First Search algorithm took " + (endNano - startNano) + " nanoseconds.");
+        return results;
     }
 
-    public static boolean searchPath(int[][] maze, int x, int y, List<Integer> path) {
+    public static boolean searchPath(int[][] maze, int x, int y, List<Point> path) {
         if (maze[y][x] == 9) {
-            path.add(x);
-            path.add(y);
+            path.add(new Point(x, y));
             return true;
         }
 
@@ -34,18 +34,15 @@ public class DepthFirst {
             int dx = 0;
             int dy = 1;
             if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+                path.add(new Point(x, y));
                 return true;
             }
 
-            // TODO: needs to calculate which way is shorter if it's impossible to go down
             // Try moving up
             dx = 0;
             dy = -1;
             if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+                path.add(new Point(x, y));
                 return true;
             }
 
@@ -53,8 +50,7 @@ public class DepthFirst {
             dx = 1;
             dy = 0;
             if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+                path.add(new Point(x, y));
                 return true;
             }
 
@@ -62,11 +58,11 @@ public class DepthFirst {
             dx = -1;
             dy = 0;
             if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+                path.add(new Point(x, y));
                 return true;
             }
         }
         return false;
     }
 }
+
