4506e1d7972c3778f3e3dc6d075ebf41eec363b5
elsaneth <elisabeth.suits@gmail.com>
2023-12-13 21:34:28 +0200

    use points instead of array

--- mazesolver/BreadthFirst.java
+++ mazesolver/BreadthFirst.java
@@ -2,61 +2,63 @@ package mazesolver;
 
 import java.util.*;
 
+import static mazesolver.View.DESTINATION;
+import static mazesolver.View.WALL;
+
 public class BreadthFirst {
-    public static List<Integer> getPath(int[][] maze) {
+
+    private static final int VISITED = 2;
+
+    public static List<Point> getPoints(int[][] maze) {
         int[][] mazeClone = new int[maze.length][];
         for (int i = 0; i < maze.length; i++) {
             mazeClone[i] = maze[i].clone();
         }
-        List<Integer> mutablePath = new ArrayList<>();
+        List<Point> results = new ArrayList<>();
         long startNano = System.nanoTime();
-        searchPath(mazeClone, 1, 1, mutablePath);
+        searchPath(mazeClone, 1, 1, results);
         long endNano = System.nanoTime();
         System.out.println("Breadth First Search algorithm took " + (endNano - startNano) + " nanoseconds.");
-        return mutablePath;
+        return results;
     }
 
 
-    public static void searchPath(int[][] maze, int startX, int startY, List<Integer> path) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(startX, startY, null));
+    public static void searchPath(int[][] maze, int startX, int startY, List<Point> path) {
+        Queue<BPoint> queue = new LinkedList<>();
+        queue.add(new BPoint(startX, startY, null));
 
         while (!queue.isEmpty()) {
-            Point current = queue.poll();
-//            System.out.println("Visiting cell: (" + current.x + ", " + current.y + ")");
-
-            if (maze[current.y][current.x] == 9) {
-//                System.out.println("Destination reached!");
+            BPoint current = queue.poll();
+            if (maze[current.y][current.x] == DESTINATION) {
                 reconstructPath(startX, startY, current, path);
                 return;
             }
             // check if right neighnor is free
             if (isFree(maze, current.x + 1, current.y)) {
                 // mark it as visited
-                maze[current.y][current.x] = 2;
+                maze[current.y][current.x] = VISITED;
                 // new point for right neighbor
-                Point next = new Point(current.x + 1, current.y, current);
+                BPoint next = new BPoint(current.x + 1, current.y, current);
                 queue.add(next);
             }
             // check if left neighbor is free
             if (isFree(maze, current.x - 1, current.y)) {
-                maze[current.y][current.x] = 2;
-                Point next = new Point(current.x - 1, current.y, current);
+                maze[current.y][current.x] = VISITED;
+                BPoint next = new BPoint(current.x - 1, current.y, current);
                 queue.add(next);
             }
             // check if upper neighbor is free
             if (isFree(maze, current.x, current.y + 1)) {
-                maze[current.y][current.x] = 2;
-                Point next = new Point(current.x, current.y + 1, current);
+                maze[current.y][current.x] = VISITED;
+                BPoint next = new BPoint(current.x, current.y + 1, current);
                 queue.add(next);
             }
             // check if neighbor below is free
             if (isFree(maze, current.x, current.y - 1)) {
-                maze[current.y][current.x] = 2;
-                Point next = new Point(current.x, current.y - 1, current);
+                maze[current.y][current.x] = VISITED;
+                BPoint next = new BPoint(current.x, current.y - 1, current);
                 queue.add(next);
             }
-//            printMazeState(maze);
         }
     }
 //    private static void printMazeState(int[][] maze) {
@@ -70,33 +72,28 @@ public class BreadthFirst {
 //        System.out.println("--------------------");
 //    }
 
-    private static void reconstructPath(int startX, int startY, Point end, List<Integer> path) {
-        Point current = end;
+    private static void reconstructPath(int startX, int startY, BPoint end, List<Point> path) {
+        BPoint current = end;
         while (current.x != startX || current.y != startY) {
-            path.add(current.x);
-            path.add(current.y);
+            path.add(new Point(current.x, current.y));
             current = current.parent;
-//            System.out.println("Path: " + path);
         }
-        path.add(startX);
-        path.add(startY);
-//        System.out.println("Path: " + path);
-
+        path.add(new Point(startX, startY));
         Collections.reverse(path);
-//        System.out.println("Path: " + path);
     }
+
     // check if position is free or it's destination
     private static boolean isFree(int[][] maze, int x, int y) {
-        return (x >= 1 && x < maze[0].length && y >= 1 && y < maze.length && (maze[y][x] == 0 || maze[y][x] == 9));
+        return (x >= 1 && x < maze[0].length && y >= 1 && y < maze.length && (maze[y][x] != WALL || maze[y][x] == DESTINATION));
     }
 
     // linked list like structure
-    private static class Point {
+    private static class BPoint {
         int x;
         int y;
-        Point parent;
+        BPoint parent;
 
-        public Point(int x, int y, Point parent) {
+        public BPoint(int x, int y, BPoint parent) {
             this.x = x;
             this.y = y;
             this.parent = parent;
